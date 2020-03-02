f076c35adab3a81555b2abc102ab0466cfca7f44
StephanHCB <sbs_github_u43a@packetloss.de>
2020-03-02 22:19:01 +0100

    statically serve swagger-ui

---
+++ third_party/swagger_ui/README.txt
@@ -0,0 +1,7 @@
+extracted from https://github.com/swagger-api/swagger-ui
+(found under dist)
+
+See LICENSE for original license information.
+
+Many thanks to the original authors for making this free software.
+
--- web/server.go
+++ web/server.go
@@ -16,6 +16,11 @@ func Serve() {
 
 	_ = emailctl.Create(server)
 
+	// TODO move this out to a static files controller
+	// serve swagger-ui and swagger.json
+	server.Static("/swagger-ui/", "third_party/swagger_ui")
+	server.StaticFile("swagger.json", "docs/swagger.json")
+
 	err := server.Run(config.ServerAddress())
 	if err != nil {
 		// TODO log a warning on intentional shutdown, and an error otherwise
